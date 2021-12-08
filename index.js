a9fab6f9834925a7fb3451e9ba276a1b1139329f
Daniel Escalona <daniel07escalona@gmail.com>
2021-12-08 13:50:41 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(number1, number2){
+    return (number1+number2);
+}
+
+function subtract(number1, number2){
+    return (number1-number2);
+}
+
+function multiply(number1, number2){
+    return (number1*number2);
+}
+
+function divide(number1, number2){
+    return (number1/number2);
+}
+
+function increment(n){
+    return (++n);
+}
+
+function decrement(n){
+    return (--n);
+}
+
+function makeInt(n){
+    return (parseInt(n, 10));
+}
+
+function makeFloat(n){
+    return (parseFloat(n));
+}
+
+function preserveDecimal(n) {
+    return (parseFloat(n));
+}
